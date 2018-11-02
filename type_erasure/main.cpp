74e6f2a27cc13d127c869ac8c1e483ae75bdefec
andrei <andrei.herdt@gmail.com>
2018-11-02 20:18:12 +0100

    Static type erasure

--- type_erasure/main.cpp
+++ type_erasure/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+
+class Lidar {
+   public:
+    void Send() const { std::cout << "Lidar" << std::endl; }
+};
+
+class Radar {
+   public:
+    void Send() const { std::cout << "Radar" << std::endl; }
+};
+
+template <typename T>
+class Sensor(T& sensor) : sensor_(sensor) {
+    struct Concept {
+        virtual ~Concept() {}
+        virtual void Send() const = 0;
+    }
+
+    template <typename T>
+    struct Model : Concept {
+        Model(
+
+    }
+
+    T sensor_;
+}
 
 int main() {
-    std::cout << "sdfa" << std::endl;
+    std::vector<Lidar> sensors;
+    sensors.push_back(Lidar{});
+    sensors.push_back(Lidar{});
+    // sensors.push_back(Radar{});
+
+    for (const auto& sensor : sensors) {
+        sensor.Send();
+    };
     return 0;
 }
